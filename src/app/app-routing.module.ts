d5863a495a813fe2804fe9982cb83dfe6c3abec8
MPH Dev <mphdevelopmentllc@MPHs-iMac.local>
2022-09-07 16:56:45 -0600

    firebase hosting & home module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  { path: '', 
+  loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+  pathMatch: 'full' 
+},
+  { path: 'home', 
+  loadChildren: () => import('./home/home.module').then(m => m.HomeModule) 
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
